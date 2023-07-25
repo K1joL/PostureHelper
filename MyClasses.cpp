1bfa0bfb67217e5809b97a683c7c8a1f1e3db8c2
K1joL <kzk19@mail.ru>
2023-07-25 13:27:37 +0300

    Updated classes Added MessageSocket classes

--- MyClasses.cpp
+++ MyClasses.cpp
@@ -8,6 +8,22 @@ MyTimer::MyTimer(QObject *parent)
 void MyTimer::startTimer(int interval)
 {
     m_timer->start(interval);
+    m_oldInterval = interval;
+}
+
+void MyTimer::pause()
+{
+    remaining = m_timer->remainingTime();
+    m_timer->stop();
+}
+
+void MyTimer::resume()
+{
+    if (remaining < 0) return;
+    m_timer->setInterval(remaining);
+    m_timer->setSingleShot(true);
+    QObject::connect(m_timer, &QTimer::timeout, this, &MyTimer::reset);
+    m_timer->start();
 }
 
 void MyTimer::increaseTime()
@@ -17,6 +33,8 @@ void MyTimer::increaseTime()
     {
         m_ss = 0;
         m_mm++;
+        if(m_mm % 5 == 0)
+            emit sig_increasePoints();
     }
     if(m_mm >= 60)
     {
@@ -29,10 +47,32 @@ QString MyTimer::getTime()
 {
     return QString("%1").arg(m_hh, 2, 10, QChar('0')) + ":" +
            QString("%1").arg(m_mm, 2, 10, QChar('0')) + ":" +
-           QString("%1").arg(m_ss, 2, 10, QChar('0'));
+            QString("%1").arg(m_ss, 2, 10, QChar('0'));
 }
 
-SidePanelAnimHelper::SidePanelAnimHelper(QMainWindow *mainWind, QWidget *sidePanel, QPushButton *btn)
+void MyTimer::changeInterval(int newInterval)
+{
+    m_oldInterval = newInterval;
+    m_timer->setInterval(newInterval);
+}
+
+void MyTimer::changeSingleShot(bool newSingleShot)
+{
+    m_singleShot = newSingleShot;
+    m_timer->setSingleShot(newSingleShot);
+}
+
+void MyTimer::reset()
+{
+    m_timer->setInterval(m_oldInterval);
+    m_timer->setSingleShot(m_singleShot);
+    QObject::disconnect(m_timer, &QTimer::timeout, this, &MyTimer::reset);
+    if (!m_singleShot) {
+        m_timer->start();
+    }
+}
+
+SidePanelAnimHelper::SidePanelAnimHelper(QWidget *mainWind, QWidget *sidePanel, QPushButton *btn)
     : m_mainWind(mainWind), m_sidePanel(sidePanel), m_btn(btn)
 {
 
@@ -50,6 +90,9 @@ SidePanelAnimHelper::SidePanelAnimHelper(QMainWindow *mainWind, QWidget *sidePan
         m_sidePanelAnim->addAnimation(m_btnAnim);
         m_panelAnim->setDuration(300);
         m_btnAnim->setDuration(300);
+    }else
+    {
+        qDebug() << "One or more arguments are nullptr!";
     }
 }
 
@@ -61,28 +104,20 @@ void SidePanelAnimHelper::hideShowPanel()
         //show panel
         aleft = m_mainWind->rect().width()-m_sidePanel->geometry().width()-49;
         m_isSidePanelHide = false;
+        m_btn->setText(">");
     }
     else
     {
         //hide panel
         aleft = m_mainWind->rect().width();
         m_isSidePanelHide = true;
+        m_btn->setText("<");
     }
-//    m_panelAnim->setStartValue(
-//                QRect(m_sidePanel->geometry().left(),
-//                      m_sidePanel->geometry().top(),
-//                      m_sidePanel->width(),
-//                      m_sidePanel->height()));
     m_panelAnim->setEndValue(
                 QRect(aleft,
                       m_sidePanel->y(),
                       m_sidePanel->width(),
                       m_sidePanel->height()));
-//    m_btnAnim->setStartValue(
-//                QRect(m_btn->x(),
-//                      m_btn->y(),
-//                      m_btn->width(),
-//                      m_btn->height()));
     m_btnAnim->setEndValue(
                 QRect(m_panelAnim->endValue().toRect().left()-m_btn->width()+1,
                       m_btn->geometry().top(),
@@ -91,23 +126,30 @@ void SidePanelAnimHelper::hideShowPanel()
     m_sidePanelAnim->start();
 }
 
-FullscreenCamAnimHelper::FullscreenCamAnimHelper(QMainWindow *mainWindow, QLabel *videocamFeed,
-                                                 QPushButton *updateCamBtn, QPushButton *fullscreenBtn)
-    : m_mainWind(mainWindow), m_videoCamFeed(videocamFeed), m_updCamBtn(updateCamBtn), m_fullscrnBtn(fullscreenBtn)
+FullscreenCamAnimHelper::FullscreenCamAnimHelper(QWidget *mainWidget, QWidget *videocamFeed, QPushButton *updateCamBtn,
+                                                 QPushButton *fullscreenBtn):
+      m_mainWidget(mainWidget),
+      m_videoCamFeed(videocamFeed),
+      m_updCamBtn(updateCamBtn),
+      m_fullscrnBtn(fullscreenBtn)
 {
+    int duration = 500;
     if(m_videoCamFeed && m_updCamBtn && m_fullscrnBtn)
     {
+        m_mainWidgetAnim = new QPropertyAnimation(m_mainWidget, "size");
         m_videoCamFeedAnim = new QPropertyAnimation(m_videoCamFeed, "size");
         m_updCamBtnAnim = new QPropertyAnimation(m_updCamBtn, "size");
         m_fullscrnBtnAnim = new QPropertyAnimation(m_fullscrnBtn, "geometry");
         m_fullscreenAnim = new QParallelAnimationGroup;
         //ANIMATION DURATION
-        m_updCamBtnAnim->setDuration(500);
-        m_fullscrnBtnAnim->setDuration(500);
-        m_videoCamFeedAnim->setDuration(500);
+        m_mainWidgetAnim->setDuration(duration);
+        m_videoCamFeedAnim->setDuration(duration);
+        m_updCamBtnAnim->setDuration(duration);
+        m_fullscrnBtnAnim->setDuration(duration);
+        m_fullscreenAnim->addAnimation(m_mainWidgetAnim);
+        m_fullscreenAnim->addAnimation(m_videoCamFeedAnim);
         m_fullscreenAnim->addAnimation(m_updCamBtnAnim);
         m_fullscreenAnim->addAnimation(m_fullscrnBtnAnim);
-        m_fullscreenAnim->addAnimation(m_videoCamFeedAnim);
     }
     else
     {
@@ -123,31 +165,177 @@ void FullscreenCamAnimHelper::onOffFullscreen()
     int fullscrnBtnTop = 0;
     if(m_isFullscreenOn)
     {
-        widthOffset -= 560;
-        heightOffset -= 370;
+        widthOffset -= 561;
+        heightOffset -= 371;
         fullscrnBtnLeft = widthOffset - m_fullscrnBtn->width();
         fullscrnBtnTop = heightOffset - m_fullscrnBtn->height();
         m_isFullscreenOn = false;
     }else
     {
-        widthOffset += 560;
-        heightOffset += 370;
+        widthOffset += 561;
+        heightOffset += 371;
         fullscrnBtnLeft = widthOffset - m_fullscrnBtn->width();
         fullscrnBtnTop = heightOffset - m_fullscrnBtn->height();
         m_isFullscreenOn = true;
     }
 
-//    m_videoCamFeedAnim->setStartValue(QSize(m_videoCamFeed->width(),m_videoCamFeed->height()));
+    m_mainWidgetAnim->setEndValue(QSize(widthOffset,heightOffset));
     m_videoCamFeedAnim->setEndValue(QSize(widthOffset,heightOffset));
-//    m_updCamBtnAnim->setStartValue(QSize(m_updCamBtn->width(),m_updCamBtn->height()));
     m_updCamBtnAnim->setEndValue(QSize(widthOffset,heightOffset));
-//    m_fullscrnBtnAnim->setStartValue(QRect(m_fullscrnBtn->x(),
-//                                           m_fullscrnBtn->y(),
-//                                           m_fullscrnBtn->width(),
-//                                           m_fullscrnBtn->height()));
     m_fullscrnBtnAnim->setEndValue(QRect(fullscrnBtnLeft,
                                          fullscrnBtnTop,
                                          m_fullscrnBtn->width(),
                                          m_fullscrnBtn->height() ));
     m_fullscreenAnim->start();
+}
+
+UpdateCamButton::UpdateCamButton(QPushButton *updateCamButton, MJPEGStreamer *camWidget)
+{
+    //Animation updateCamBtn
+    QIcon updCamBtnIcon(":/images/iconupdbtn.png");
+    updateCamButton->setIconSize(QSize(48,48));
+    updateCamButton->setIcon(updCamBtnIcon);
+    m_updateCamBtnAnim = new QMovie(":/images/gifupdbtn.gif");
+    connect(m_updateCamBtnAnim, &QMovie::frameChanged, [=]{
+      updateCamButton->setIcon(m_updateCamBtnAnim->currentPixmap());
+    });
+    connect(camWidget, &MJPEGStreamer::streaming, [=]{
+        m_updateCamBtnAnim->stop();
+        updateCamButton->setIcon(updCamBtnIcon);
+        updateCamButton->hide();
+    });
+    connect(camWidget, &MJPEGStreamer::disconnected, [=]{
+        updateCamButton->show();
+    });
+}
+
+void UpdateCamButton::startAnim()
+{
+    m_updateCamBtnAnim->start();
+}
+
+MessageSocket::MessageSocket(QHostAddress address, quint16 portIn, quint16 portTo)
+    :
+      m_socket(new QUdpSocket),
+      m_address(address),
+      m_portIn(portIn),
+      m_portTo(portTo)
+{
+    m_socket->bind(m_address, m_portIn);
+    connect(m_socket, SIGNAL(readyRead()), this, SLOT(read()));
+    initMaps();
+}
+
+void MessageSocket::read()
+{
+    QByteArray data;
+    data.resize(m_socket->pendingDatagramSize());
+    m_socket->readDatagram(data.data(), data.size(), &m_address);
+    QString str(QString::fromUtf8(data));
+    processData(QString(str));
+}
+
+void MessageSocket::send(QString message)
+{
+    m_socket->writeDatagram(message.toUtf8(), m_address, m_portTo);
+}
+
+/*
+*   command structure: "cmd value ..."
+*   cmd types:
+*       Error
+*       Ex (exercise)
+*   Ex value types:
+*       Ready
+*       NotReady
+*       Start
+*       Did
+*   Error value types:
+*       Neck
+*       Back
+*       LeftLeg
+*       RightLeg
+*       None
+*/
+void MessageSocket::processData(const QString message)
+{
+    QStringList bufferList = message.split(" ", QString::SkipEmptyParts);
+    if(bufferList.size() == 1 || bufferList.size() == 0)
+        return;
+    int cmd = *m_cmdMap.find(bufferList.at(0));
+    //change first element to none to clean all labels first
+    bufferList[0] = ("None");
+    int value = 0;
+    switch(cmd)
+    {
+        case Error:
+            for(auto &listValue : bufferList)
+            {
+                value = *m_errorValueMap.find(listValue);
+                qDebug() << value << " " << listValue;
+                switch(value)
+                {
+                    case Neck:
+                        emit sig_error_neck_received();
+                        break;
+                    case Back:
+                        emit sig_error_back_received();
+                        break;
+                    case LeftLeg:
+                        emit sig_error_lLeg_received();
+                        break;
+                    case RightLeg:
+                        emit sig_error_rLeg_received();
+                        break;
+                    case None:
+                        emit sig_error_none_received();
+                        break;
+                    default:
+                        qDebug() << "Wrong Error command value!";
+                        return;
+                }
+            }
+            break;
+        case Ex:
+            value = *m_exValueMap.find(bufferList.at(1));
+            switch(value)
+            {
+            case Start:
+                emit sig_exercise_start_received();
+                break;
+            case Ready:
+                emit sig_exercise_ready_received();
+                break;
+            case NotReady:
+                emit sig_exercise_notReady_received();
+                break;
+            case Did:
+                emit sig_exercise_did_received();
+                break;
+            default:
+                qDebug() << "Wrong Ex command value!";
+            }
+            break;
+        default:
+        qDebug() << "Wrong cmd";
+        return;
+    }
+}
+
+void MessageSocket::initMaps()
+{
+    //cmd
+    m_cmdMap.insert("Error", cmdEnum::Error);
+    m_cmdMap.insert("Ex", cmdEnum::Ex);
+    //Error value
+    m_errorValueMap.insert("None", errorValueEnum::None);
+    m_errorValueMap.insert("Neck", errorValueEnum::Neck);
+    m_errorValueMap.insert("Back", errorValueEnum::Back);
+    m_errorValueMap.insert("LeftLeg", errorValueEnum::LeftLeg);
+    m_errorValueMap.insert("RightLeg", errorValueEnum::RightLeg);
+    //Ex value
+    m_exValueMap.insert("Start", exValueEnum::Start);
+    m_exValueMap.insert("Ready", exValueEnum::Ready);
+    m_exValueMap.insert("NotReady", exValueEnum::NotReady);
+    m_exValueMap.insert("Did", exValueEnum::Did);
 }
--- MyClasses.h
+++ MyClasses.h
@@ -8,28 +8,42 @@
 #include <QParallelAnimationGroup>
 #include <QtWidgets>
 #include <QPushButton>
-#include <QNetworkAccessManager>
-#include <QNetworkRequest>
-#include <QNetworkReply>
-#include <QByteArray>
 #include <QPixmap>
+#include <QUdpSocket>
+#include <QNetworkDatagram>
+#include <QMap>
+
+#include "mjpegstreamer.h"
 
 class MyTimer : public QObject
 {
     Q_OBJECT
+
+public:
+    MyTimer(QObject *parent = nullptr);
+    virtual ~MyTimer(){ delete m_timer; }
+    QTimer *getTimer() { return m_timer; }
+    void startTimer(int interval = 0);
+    void stopTimer() { m_timer->stop(); }
+    void pause();
+    void resume();
+    void increaseTime();
+    QString getTime();
+private:
+    void changeInterval(int newInterval);
+    void changeSingleShot(bool newSingleShot);
+    void reset();
+signals:
+    void sig_increasePoints();
+
 private:
     QTimer *m_timer;
+    int remaining;
+    int m_oldInterval;
+    bool m_singleShot = false;
     int m_hh = 0;
     int m_mm = 0;
     int m_ss = 0;
-
-public:
-    MyTimer(QObject *parent);
-    ~MyTimer(){ delete m_timer; }
-    QTimer *getTimer() { return m_timer; }
-    void startTimer(int interval);
-    void increaseTime();
-    QString getTime();
 };
 
 class SidePanelAnimHelper : public QObject
@@ -39,13 +53,14 @@ private:
     QPropertyAnimation *m_panelAnim;
     QPropertyAnimation *m_btnAnim;
     QParallelAnimationGroup *m_sidePanelAnim;
-    QMainWindow *m_mainWind;
+    QWidget *m_mainWind;
     QWidget *m_sidePanel;
     QPushButton *m_btn;
     bool m_isSidePanelHide = true;
 
 public:
-    SidePanelAnimHelper(QMainWindow *mainWind, QWidget *sidePanel, QPushButton *btn);
+    SidePanelAnimHelper(QWidget *mainWind, QWidget *sidePanel, QPushButton *btn);
+    virtual ~SidePanelAnimHelper(){}
 
 public slots:
     void hideShowPanel();
@@ -55,21 +70,95 @@ public slots:
 class FullscreenCamAnimHelper : public QObject
 {
     Q_OBJECT
+public:
+    FullscreenCamAnimHelper(QWidget *mainWidget, QWidget *videocamFeed,
+                        QPushButton *updCamBtn, QPushButton *fullscreenBtn);
+    virtual ~FullscreenCamAnimHelper(){}
+
+public slots:
+    void onOffFullscreen();
+
 private:
-    QPropertyAnimation *m_fullscrnBtnAnim;
+    QPropertyAnimation *m_mainWidgetAnim;
     QPropertyAnimation *m_videoCamFeedAnim;
     QPropertyAnimation *m_updCamBtnAnim;
+    QPropertyAnimation *m_fullscrnBtnAnim;
     QParallelAnimationGroup *m_fullscreenAnim;
-    QMainWindow *m_mainWind;
-    QLabel *m_videoCamFeed;
+    QWidget *m_mainWidget;
+    QWidget *m_videoCamFeed;
     QPushButton *m_fullscrnBtn;
     QPushButton *m_updCamBtn;
     bool m_isFullscreenOn = false;
+};
+
+class ExerciseBlockAnimHelper : public QObject
+{
+    Q_OBJECT
 public:
-    FullscreenCamAnimHelper(QMainWindow *mainWindow, QLabel *videocamFeed,
-                        QPushButton *updCamBtn, QPushButton *fullscreenBtn);
+    ExerciseBlockAnimHelper(QWidget *avalExerWidget);
+    virtual ~ExerciseBlockAnimHelper(){}
 public slots:
-    void onOffFullscreen();
+    void hideShowBlock();
+
+private:
+    QPropertyAnimation m_blockAnim;
+    QWidget *avalExerWidget;
+};
+
+class UpdateCamButton : public QObject
+{
+    Q_OBJECT
+public:
+    UpdateCamButton(QPushButton *updateCamButton, MJPEGStreamer *camWidget);
+    void startAnim();
+private:
+    QMovie *m_updateCamBtnAnim;
+};
+
+class MessageSocket : public QUdpSocket
+{
+    Q_OBJECT
+public:
+    MessageSocket(QHostAddress address, quint16 portIn, quint16 portTo);
+    void send(QString message);
+    void processData(const QString message);
+    void initMaps();
+signals:
+    void sig_error_back_received();
+    void sig_error_neck_received();
+    void sig_error_lLeg_received();
+    void sig_error_rLeg_received();
+    void sig_error_none_received();
+    void sig_exercise_did_received();
+    void sig_exercise_start_received();
+    void sig_exercise_ready_received();
+    void sig_exercise_notReady_received();
+public slots:
+    void read();
+
+private:
+    enum cmdEnum{ Error,
+                  Ex,
+                  MAX_CMD};
+    enum errorValueEnum{ None,
+                         Neck,
+                         Back,
+                         LeftLeg,
+                         RightLeg,
+                         MAX_ERROR_VALUE};
+    enum exValueEnum{ Start,
+                      Ready,
+                      NotReady,
+                      Did,
+                      MAX_EX_VALUE};
+
+    QUdpSocket *m_socket;
+    QHostAddress m_address;
+    quint16 m_portIn;
+    quint16 m_portTo;
+    QMap<QString, int> m_cmdMap;
+    QMap<QString, int> m_errorValueMap;
+    QMap<QString, int> m_exValueMap;
 };
 
 #endif // MYCLASSES_H
